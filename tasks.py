f64ca6892318d8db0be30625cf1dadb4e777aad0
DeeCaaD <perttu.kangas@hotmail.fi>
2022-11-24 12:59:51 +0200

    Prep pipeline for coverage (<80%) #skip

--- tasks.py
+++ tasks.py
@@ -5,21 +5,21 @@ from invoke import task
 def dev(ctx):
     ctx.run("flask run", pty=True)
 
-
 @task
 def start(ctx):
     ctx.run("gunicorn --bind 0.0.0.0:8080 app:app", pty=True)
-
 
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
-
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest && coverage html", pty=True)
 
+#@task
+#def coverage_report(ctx):
+#    ctx.run("coverage report --fail-under=80", pty=True)
 
 @task
 def lint(ctx):
