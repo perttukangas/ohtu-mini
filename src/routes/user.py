c06bcb6bad1293ebcfcff36ad6794f79726e60c7
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2022-11-25 17:14:57 +0200

    add account creation

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from ..services import user
+from flask import render_template, request, redirect
 from app import app
 
 @app.route("/login")
@@ -6,3 +7,25 @@ def login():
     # Metodin nimi pitää olla uniikki
     # Ei saa esiintyä muissakaan routeissa
     return render_template("test.html")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            print("väärä salasana")
+        if user.register(username, password1):
+            print("luotiin")
+            return redirect("/")
+
+# TÄMÄ ON VAIN TESTAAMISTA VARTEN!!!
+@app.route("/showall")
+def showall():
+    user.show_users()
+    return redirect("/register")
--- src/services/user.py
+++ src/services/user.py
@@ -5,17 +5,31 @@ from src.utils.db import connect
 
 
 def register(username, password):
-  # ...
 
-  hash_value = generate_password_hash(password)
+    hash_value = generate_password_hash(password)
+    try:
+        query = "INSERT INTO Users (username, password) VALUES (:username,:password)"
+        con = connect()
+        con.run(query, username=username, password=hash_value)
+        con.close()
+        print("services.register done")
+    except:
+        con.close()
+        return False
 
-  # ...
-
-  return True
+    return True
 
 def login(username, password):
   
-  # ...
+    query = "SELECT id, username, password FROM Users WHERE username=:username"
+    #result = 
 
-  session["csrf_token"] = os.urandom(16).hex()
-  return True
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
+
+# TESTAAMISTA VARTEN !!!
+def show_users():
+    con = connect()
+    for row in con.run("SELECT * FROM Users"):
+        print(row)
+    con.close()
