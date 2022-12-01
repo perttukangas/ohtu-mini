032f0f9ec73df1ebfe8e5003505f80b776d3ca19
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2022-12-01 14:24:36 +0200

    switch to using dbapi

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,8 +1,9 @@
 import os
-import pg8000.native
+import pg8000.dbapi
 
 def connect():
-  return pg8000.native.Connection(os.environ.get("DB_USERNAME"),
+  return pg8000.dbapi.connect(
+    user=os.environ.get("DB_USERNAME"),
     host=os.environ.get("DB_HOST"),
     database=os.environ.get("DB_DATABASE"), 
     port=os.environ.get("DB_PORT"),
