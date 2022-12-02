b62de2236e8ba34b7c1a1ec51b227e18310d5373
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-02 12:00:58 +0200

    Refactoring ref methods

--- src/routes/reference.py
+++ src/routes/reference.py
@@ -6,14 +6,7 @@ from ..utils import reference_type
 
 @app.route("/add/<type_name>")
 def add_redirect(type_name):
-    type = reference_type.ReferenceType[type_name]
-    required = type.get_required_for_add()
-    optional = type.get_optional_for_add()
-
-    print(required)
-    print(optional)
-
-    return render_template("add.html", type=(type_name, type.get_name()), required=required, optional=optional)
+    return get_add_page(type_name, "")
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -21,76 +14,62 @@ def add():
     required = ref_type.get_required()
     optional = ref_type.get_optional()
 
-    print(ref_type)
-    print(required)
-    print(optional)
-
     columns = []
     values = []
 
-    for req in required:
-        form_data = None
-        form_req = None
-        if type(req) is tuple:
-            both_used = validata_or(request.form[req[0]], request.form[req[1]])
-            if both_used is not None:
-                print(both_used)
-                return redirect("/")
+    ref_name = ref_type.name
 
-            form_data = request.form[req[0]] if len(request.form[req[0]]) > 0 else request.form[req[1]]
-            form_req = req[0] if len(request.form[req[0]]) > 0 else req[1]
-        else:
-            form_data = request.form[req]
-            form_req = req 
-        
-        if len(form_data) < 1:
-            print(f"Required: {req}")
-            return redirect("/")
-        
-        error = validate(form_req, form_data)
-        if error is not None:
-            print(error)
-            return redirect("/")
-
-        columns.append(form_req)
-        values.append(form_data)
+    required_validation = validate_input(ref_name, required, columns, values, True)
+    if required_validation is not None:
+        return required_validation
     
-    for opt in optional:
-        form_data = None
-        form_opt = None
-        if type(opt) is tuple:
-            both_used = validata_or(request.form[opt[0]], request.form[opt[1]])
-            if both_used is not None:
-                print(both_used)
-                return redirect("/")
-
-            form_data = request.form[opt[0]] if len(request.form[opt[0]]) > 0 else request.form[opt[1]]
-            form_opt = opt[0] if len(request.form[opt[0]]) > 0 else opt[1]
-        else:
-            form_data = request.form[opt]
-            form_opt = opt
-        
-        if len(form_data) > 0:
-            error = validate(form_opt, form_data)
-            if error is not None:
-                print(error)
-                return redirect("/")
-        
-            columns.append(form_opt)
-            values.append(form_data)
-
+    optional_validation = validate_input(ref_name, optional, columns, values, False)
+    if optional_validation is not None:
+        return optional_validation
 
     user_id = session["user_id"]
     ref_id = request.form["reference_id"]
-    ref_name = ref_type.name
 
     reference.add_reference(user_id, ref_id, ref_name, columns, values)
 
     return redirect("/")
 
+def get_add_page(type_name, message):
+    type = reference_type.ReferenceType[type_name]
+    required = type.get_required_for_add()
+    optional = type.get_optional_for_add()
+    return render_template("add.html", type=(type_name, type.get_name()), required=required, optional=optional, message=message)
 
-def validata_or(form1, form2):
-    return f"Vain toinen kentistä {form1} ja {form2} voi sisältää tietoa" if len(form1) > 0 and len(form2) > 0 else None
+def validate_input(ref_type, types, columns, values, required):
+    for cur_type in types:
+
+        form_resp = get_form_data(cur_type)
+        if len(form_resp) != 2:
+            return get_add_page(ref_type, form_resp)
+        
+        form_type, form_data = form_resp
+        
+        if len(form_data) < 1 and required:
+            return get_add_page(ref_type, f"Vaadittu kenttä täyttämättä: {cur_type}")
+        
+        error = validate(form_type, form_data)
+        if error is not None:
+            return get_add_page(ref_type, error)
+
+        columns.append(form_type)
+        values.append(form_data)
+
+def get_form_data(cur_type):
+    if type(cur_type) is tuple:
+        if len(request.form[cur_type[0]]) > 0 and len(request.form[cur_type[1]]) > 0:
+            return f"Vain toinen kentistä {cur_type[0]} ja {cur_type[1]} voi sisältää tietoa"
+
+        form_type = cur_type[0] if len(request.form[cur_type[0]]) > 0 else cur_type[1]
+        form_data = request.form[form_type]
+    else:
+        form_type = cur_type 
+        form_data = request.form[cur_type]
+    return (form_type, form_data)
 
 def validate(row_type, data):
     
--- src/services/reference.py
+++ src/services/reference.py
@@ -3,16 +3,9 @@ from src.utils.db import connect
 from ..utils import reference_type
 
 def add_reference(user_id, ref_id, ref_name, columns, values):
-    print(user_id, ref_id, ref_name, columns, values)
-
     column = ", ".join(columns)
     formatter = ", ".join("%s" for _ in range(len(columns)))
     sql = f"INSERT INTO tblReference (user_id, reference_id, reference_name, {column}) VALUES (%s, %s, %s, {formatter})"
-
-    print(column)
-    print(formatter)
-    print(sql)
-    print((user_id, ref_id, ref_name) + tuple(values))
 
     con = connect()
     cur = con.cursor()
@@ -23,8 +16,6 @@ def add_reference(user_id, ref_id, ref_name, columns, values):
     con.close()
 
 def get_references(user_id):
-
-    print(user_id)
 
     con = connect()
     cur = con.cursor()
