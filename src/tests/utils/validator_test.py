9709843606b947f27ee39d2cb80df2e4a4686073
DeeCaaD <perttu.kangas@hotmail.fi>
2022-12-07 16:41:42 +0200

    Refactor input validator

--- src/tests/utils/validator_test.py
+++ src/tests/utils/validator_test.py
@@ -1,24 +1,59 @@
 import unittest
-from src.utils.validator import Validator, check_for_errors
+from src.utils.validator import Validator
 
-class TestValidatorTest(unittest.TestCase):
-    def setUp(self):
-        self.validator = Validator()
+class TestValidator(unittest.TestCase):
+    
+    def test_required_fields_not_filled(self):
+        data = dict(
+                reference_id="uniq1",
+                reference_name="ARTICLE",
+                author="author",
+                journal="",
+                title="title",
+                year="15"
+            )
+        validator = Validator(data)
+        validator.run_all_validators()
+        self.assertEqual(validator.error, "Vaadittu kenttä täyttämättä: journal")
+    
+    def test_required_id_not_filled(self):
+        data = dict(
+                reference_id="",
+                reference_name="ARTICLE",
+                author="author",
+                journal="journal",
+                title="title",
+                year="15"
+            )
+        validator = Validator(data)
+        validator.run_all_validators()
+        self.assertEqual(validator.error, "Vaadittu kenttä täyttämättä: reference_id")
+    
+    def test_both_of_or_forms_filled(self):
+        data = dict(
+                reference_id="123",
+                reference_name="BOOK",
+                author="author",
+                editor="editor",
+                title="title",
+                publisher="publisher",
+                year="15"
+            )
 
-    def test_validator_object_created_successfully(self):
-        self.assertEqual(self.validator.has_errors(), False)
+        validator = Validator(data)
+        validator.run_all_validators()
+        self.assertEqual(validator.error, "Vain toinen kentistä author ja editor voi sisältää tietoa")
+    
+    def test_too_long_form(self):
+        data = dict(
+                reference_id="123",
+                reference_name="BOOK",
+                author="author",
+                title="title",
+                publisher="publisheraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                year="15"
+            )
 
-    def test_year_input_check_works_correctly(self):
-        self.validator.year_input_correctly("author", "Mika Waltari")
-        self.assertEqual(self.validator.has_errors(), False)
-        self.validator.year_input_correctly("year", "2022")
-        self.assertEqual(self.validator.has_errors(), False)
-        self.validator.year_input_correctly("year", "banaani")
-        self.assertEqual(self.validator.has_errors(), True)
-
-    def test_length_check_works_as_intended(self):
-        self.assertEqual(self.validator.has_errors(), False)
-        self.validator.has_length_less_than("author", "Aku Ankka")
-        self.assertEqual(self.validator.has_errors(), False)
-        self.validator.has_length_less_than("address", "abc"*250)
-        self.assertEqual(self.validator.has_errors(), True)
+        validator = Validator(data)
+        validator.run_all_validators()
+        self.assertEqual(validator.error, "publisher ei voi olla pidempi kuin 120 kirjainta")
--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,32 +1,103 @@
+from ..utils.reference_type import ReferenceType
+
 class Validator:
-    def __init__(self):
-        self.errors = []
-        self._max_string_length = 100
-
-    def add_error(self, error: str):
-        self.errors.append(str(error))
-
-    def has_errors(self):
-        return len(self.errors) != 0
-
-    def __str__(self):
-        return self.errors[0].capitalize()
-
-    def has_length_less_than(self, form_type, content):
-        if len(content) > self._max_string_length:
-            self.add_error(
-                f"{form_type} ei voi olla pidempi kuin {self._max_string_length} kirjainta"
-                )
+    def __init__(self, data):
+        self.data = data
+        self.error = None
+        self.columns = []
+        self.values = []
     
-    def year_input_correctly(self, form_type, content):
-        if form_type == "year":
-            if not content.isnumeric():
-                self.add_error(f"Kentän '{form_type}' arvon tulee olla kokonaisluku")
+    def run_all_validators(self):
+        self.validate_reference_name()
+        if self.error:
+            return self.error
+        
+        self.validate_reference_id()
+        if self.error:
+            return self.error
+        
+        self.validate_input_required()
+        if self.error:
+            return self.error
+        
+        self.validate_input_optional()
+        if self.error:
+            return self.error
+        
+        return None
+    
+    def validate_reference_name(self):
+        try:
+            ReferenceType[self.data.get("reference_name")]
+        except KeyError:
+            self.error = "404"
 
+    def validate_reference_id(self):
+        if len(self.data.get("reference_id", "")) < 1:
+            self.error = "Vaadittu kenttä täyttämättä: reference_id"
+    
+    def validate_input_required(self):
+        self._validate_input(True, ReferenceType[self.data.get("reference_name")])
+    
+    def validate_input_optional(self):
+        self._validate_input(False, ReferenceType[self.data.get("reference_name")])
 
-def check_for_errors(validator: Validator, form_type: str, content: str):
-    validator.has_length_less_than(form_type, content)
-    validator.year_input_correctly(form_type, content)
-    if validator.has_errors():
-        return validator
-    return None
+    def _validate_input(self, required, ref_type):
+        types = ref_type.get_required() if required else ref_type.get_optional()
+        for cur_type in types:
+
+            bib_entry = self.get_bib_entry(cur_type)
+            if self.error:
+                # Jos kummatkin "tai" kentistä on täytetty 
+                break
+
+            bib_type, bib_data = bib_entry
+
+            if len(bib_data) < 1:
+                if required:
+                    # Vaatitu kenttä on siis täyttämättä -> error
+                    self.error = f"Vaadittu kenttä täyttämättä: {cur_type}"
+                    break
+
+                # Jos valinnainen kenttä niin älä lähetä tyhjää merkkijonoa tietokantaan
+                continue
+            
+            self.validate_one_field(bib_type, bib_data)
+            if self.error:
+                # Data annetulle bib tyypille ei ollut valid
+                break
+
+            # Lisää "validit" tyypit ja arvot listoihin samassa järjestyksessä
+            self.columns.append(bib_type)
+            self.values.append(bib_data)
+
+    def get_bib_entry(self, cur_type):
+        if isinstance(cur_type, tuple):
+            first = self.data.get(cur_type[0], "") 
+            second = self.data.get(cur_type[1], "") 
+            if len(first) > 0 and len(second) > 0:
+                # Koska joillakin bib tyypeillä on mahdollista x tai y, niin tässä tarkastetaan
+                # ettei kumpikin niistä ole kentistä ole täytettynä
+                self.error = f"Vain toinen kentistä {cur_type[0]} ja {cur_type[1]} voi sisältää tietoa"
+                return None
+
+            bib_type = cur_type[0] if len(first) > 0 else cur_type[1]
+            bib_entry = first if len(first) > 0 else second
+        else:
+            # Eli kenttä on "yksittäinen", jolloin ei tarvetta tarkastaa ovatko
+            # kummatkin "tai" kentistä täytettynä
+            bib_type = cur_type
+            bib_entry = self.data.get(cur_type, "") 
+        return (bib_type, bib_entry)
+
+    def validate_one_field(self, bib_type, bib_data):
+        self.is_less_than(bib_type, bib_data, 120)
+        self.is_numeric(bib_type, bib_data)
+
+    def is_less_than(self, bib_type, bib_data, length):
+        if len(bib_data) > length:
+            self.error = f"{bib_type} ei voi olla pidempi kuin {length} kirjainta"
+    
+    def is_numeric(self, bib_type, bib_data):
+        if bib_type == "year" and not bib_data.isnumeric():
+            self.error = f"Kentän '{bib_type}' arvon tulee olla kokonaisluku"
